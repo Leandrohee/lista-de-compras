fbc4e107bcce341bf7f3c5a5a8628f9eb4879692
3061867 <70094757178@cbmdf.gov.br>
2023-05-19 12:38:38 -0300

    Adcionei a funcao comprados e comecei a funcao editar

--- main.js
+++ main.js
@@ -22,60 +22,79 @@ function addNovoItem(novoItem){
     }
     else{ 
         listaDeCompras.push({
-            nome: novoItem
+            nome: novoItem,
+            checar: false
         });
-        preencherItem()
+        mostrarItem()
     }
 }
 
-function preencherItem(){
+function mostrarItem(){
     listaDeItens.innerHTML = '';
+    itensComprados.innerHTML = '';
+
     listaDeCompras.forEach((element,index) => {
-        const novaLinha = document.createElement('li');
-        listaDeItens.appendChild(novaLinha);
-        novaLinha.innerHTML = `
-        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-        <div>
-            <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="">${element.nome}</input>
-        </div>
-        <div>
-            <i class="fa-solid fa-trash is-clickable deletar" data-deletar></i>
-        </div>
-         </li>
-        `
+
+        if(element.checar){
+            const novaLinha = document.createElement('li');
+            itensComprados.appendChild(novaLinha);
+            novaLinha.innerHTML = `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" checked class="is-clickable" />  
+                    <span class="itens-comprados is-size-5">${element.nome}</span>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar" data-deletar></i>
+                </div>
+            </li>
+            `    
+        }
+        else{
+            const novaLinha = document.createElement('li');
+            listaDeItens.appendChild(novaLinha);
+            novaLinha.innerHTML = `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" class="is-clickable" />
+                    <input type="text" class="is-size-5" value="">${element.nome}</input>
+                </div>
+                <div>
+                <i class="fa-regular fa-floppy-disk is-clickable"></i><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
+                    <i class="fa-solid fa-trash is-clickable deletar" data-deletar></i>
+                </div>
+            </li>
+            `
+        }
+        
     })
     
     var listaCheckBox = document.querySelectorAll('input[type="checkbox"]'); 
-    listaCheckBox.forEach(e => e.addEventListener("click", event => checkboxClick(event))) 
+    listaCheckBox.forEach(e => e.addEventListener("click", event => checkboxClick(event)));
 
     var listaApagar = document.querySelectorAll('[data-deletar]');
-    listaApagar.forEach(e => e.addEventListener("click", event => deletarClick(event)))
+    listaApagar.forEach(e => e.addEventListener("click", event => deletarClick(event)));
+
+    var listaEditar = document.querySelectorAll('.editar');
+    listaEditar.forEach(e => e.addEventListener('click', event => editarClick(event)));
 }
 
 
 function checkboxClick(event){
-    const itemComprado =  event.target.parentNode.innerText;
-
-    const novaLinha = document.createElement('li');
-    itensComprados.appendChild(novaLinha);
-    novaLinha.innerHTML = `
-    <li class="item-compra is-flex is-justify-content-space-between" data-value="">
-        <div>
-            <input type="checkbox" checked class="is-clickable" />  
-            <span class="itens-comprados is-size-5"></span>
-        </div>
-        <div>
-            <i class="fa-solid fa-trash is-clickable deletar"></i>
-        </div>
-    </li>
-    `
+    const nItemClicado = event.target.parentNode.parentNode.dataset.value
+    listaDeCompras[nItemClicado].checar = event.target.checked
+    mostrarItem();
 }
 
-function deletarClick (event){
+function deletarClick(event){
     const liItemDeletado = event.target.parentNode.parentNode.parentNode
     const nItemDeletado = event.target.parentNode.parentNode.dataset.value
-    console.log(nItemDeletado)
     listaDeCompras.splice(nItemDeletado,1);
     liItemDeletado.remove();
+}
+
+function editarClick(event){
+    const nItemEditado = event.target.parentElement.parentNode.dataset.value;
+    listaDeCompras.splice(nItemEditado,1,{nome:"Leandro", checar: false})
+    mostrarItem();
 }
